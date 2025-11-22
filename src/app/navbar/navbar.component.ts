f4c75a78003f4e50550b24dd2ff88ba4f51ac8bb
robertobzrr <robertobezerrafh@outlook.com>
2025-11-22 14:51:24 -0300

    introdução a components

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  @Input() nome: string = 'nome teste';
+
+}
+
+
